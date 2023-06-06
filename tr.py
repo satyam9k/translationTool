120aa86b6881b2292704ca6cc3dab10efd4f6b7c
satyam9k <75788300+satyam9k@users.noreply.github.com>
2023-06-06 21:07:03 +0530

    Update tr.py

--- tr.py
+++ tr.py
@@ -5,6 +5,7 @@ from langdetect import detect
 # Load tokenizer and model
 tokenizer = AutoTokenizer.from_pretrained("t5-base", model_max_length=150)
 model = AutoModelForSeq2SeqLM.from_pretrained("t5-base", low_cpu_mem_usage=True)
+translator = None  # Global translator variable for reusing model instance
 
 # Set maximum input length
 MAX_INPUT_LENGTH = 150
@@ -25,13 +26,25 @@ if st.button("Translate"):
         input_text = f"{input_lang} to {target_lang}: {user_input}"
         encoded_input = tokenizer.encode(input_text, return_tensors="pt", truncation=True, padding=True, max_length=150)
 
-        # Generate translation
-        translation = model.generate(encoded_input, max_length=100)
+        # Check if translator is already initialized
+        if translator is None:
+            # Create a new translator instance
+            translator = model.generate
 
-        # Decode the translated text
-        translated_text = tokenizer.decode(translation[0], skip_special_tokens=True)
+        # Generate translation
+        translation = translator(encoded_input, max_length=100)
+
+        # Clear variables and intermediate results
+        del encoded_input
+        translation_text = tokenizer.decode(translation[0], skip_special_tokens=True)
+        del translation
 
         # Display the translated text
-        st.text_area("Translated Text", value=translated_text, height=200)
+        st.text_area("Translated Text", value=translation_text, height=200)
+        del translation_text
+
+        # Clear user input after translation
+        user_input = ""
+        gc.collect()  # Force garbage collection to free memory
     else:
         st.warning("Please enter text to translate.")
